cf640bd537407b6df665626d4d1fd6922aa91dc9
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-01-10 21:09:07 +0900

    機能追加：フォーム入力時の値チェック

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -7,40 +7,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title></title>
+
+    <link rel="stylesheet" href="/css/build/login/index.css">
 </head>
 
-<body>
+<body class="login">
+    <main id="login">
+        <?php if ($isLoginActive) { ?>
+            <h2 class="title">ログイン状態</h2>
+            <a href="/login/unregister">ログアウト</a>
+        <?php } else { ?>
+            <h2 class="title">新規登録</h2>
+            <form ref="registerForm" method="post" action="/login/register">
+                @csrf
+                <div>
+                    ID : <input type="text" v-model="id" name="id">
+                </div>
+                <div>
+                    PW : <input type="password" v-model="password" name="password">
+                </div>
+                <div>
+                    <button type="button" v-on:click="registerSubmit">送信</button>
+                </div>
+            </form>
 
-    <?php if ($isLoginActive) { ?>
-    <a href="/login/unregister">ログアウト</a>
-    <?php } else { ?>
-        <h2>新規登録</h2>
-        <form method="post" action="/login/register">
-            @csrf
-            <div>
-                ID : <input type="text" name="id">
-            </div>
-            <div>
-                PW : <input type="password" name="password">
-            </div>
-            <div>
-                <input type="submit" value="送信">
-            </div>
-        </form>
-        <h2>ログイン</h2>
-        <form method="post" action="/login/sign_up">
-            @csrf
-            <div>
-                ID : <input type="text" name="id">
-            </div>
-            <div>
-                PW : <input type="password" name="password">
-            </div>
-            <div>
-                <input type="submit" value="送信">
-            </div>
-        </form>
-    <?php } ?>
+            <h2 class="title">ログイン</h2>
+            <form ref="loginForm" method="post" action="/login/sign_up">
+                @csrf
+                <div>
+                    ID : <input type="text" v-model="id" name="id">
+                </div>
+                <div>
+                    PW : <input type="password" v-model="password" name="password">
+                </div>
+                <div>
+                    <button type="button" v-on:click="loginSubmit">送信</button>
+                </div>
+            </form>
+        <?php } ?>
+    </main>
+    <script src="/js/build/login/index.js"></script>
 </body>
 
 </html>
